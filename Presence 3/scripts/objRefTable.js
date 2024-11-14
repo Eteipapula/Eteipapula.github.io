c2e8565573eb40fc3adde7b0553a37ae65d7ce3f
Eteipapula <lugulubre12345@gmail.com>
2024-11-15 00:19:19 +0100

    s
    s

--- Presence 3/scripts/objRefTable.js
+++ Presence 3/scripts/objRefTable.js
@@ -1,13 +1,15 @@
 const C3 = self.C3;
 self.C3_GetObjectRefTable = function () {
 	return [
-
+		C3.Plugins.Text,
+		C3.Behaviors.Anchor
 	];
 };
 self.C3_JsPropNameTable = [
-
+	{Ancre: 0},
+	{Texte: 0}
 ];
 
 self.InstanceType = {
-
+	Texte: class extends self.ITextInstance {}
 }
